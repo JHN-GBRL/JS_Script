f4e39ecff4b530bc0eecd95bed6d27c1cee71b01
JHN-GBRL <157443905+JHN-GBRL@users.noreply.github.com>
2024-05-17 11:06:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+
+// Declare variables for the bold and italic words
+
+let name = "Mr.John";
+let realm = "realm";
+let code = "code";
+let webWeaver = "web weaver";
+let pearls = "pearls"; 
+let alchemistOfTheBrowser = "alchemist of the browser";
+let keyboard = "keyboard";
+let fingers = "fingers";
+let keys = "keys";
+let tags = "tags";
+let HTML = "HTML";
+let symphonies = "symphonies";
+let CSSCloak = "CSS cloak";
+let styling = "Styling";
+let colors = "Colors";
+let fonts = "fonts";
+let evoke = "evoke";
+let visual = "visual";
+let JavaScriptSpells = "JavaScript spells";
+let forms = "Forms";
+let animations = "animations";
+let bugs = "bugs";
+let debuggingSpectacles = "debugging spectacles";
+let variables = "variables";
+let devices = "devices";
+let responsiveSea = "responsive sea";
+let adapting = "adapting";
+let fluidity = "fluidity";
+let labyrinth = "labyrinth";
+let SEOWhisperer = "SEO whisperer";
+let metaTags = "meta tags";
+let wanderers = "wanderers";
+let mysticalPotion = "mystical potion";
+let merge = "merge";
+let repository = "repository";
+let moon = "moon";
+let world = "world";
+let deployments = "Deployments";
+let website = "website";
+let webDeveloper = "web developer";
+let pixels = "pixels";
+let clicks = "clicks";
+let binary = "binary";
+let matrix = "matrix";
+let wand = "wand";
+let pages = "pages";
+let painting = "Painting";
+let magic = "magic";
+let will = "will";
+let propel = "propel";
+let glitches = "glitches";
+let codesarc = "code's arc";
+let tames = "tames";
+let across= "Across";
+let guide= "guide";
+let gits= "git's";
+let unfurled= "unfurled";
+
+// Concatenate these variables to form sentences
+let sentence1 = `<b># Ode to Mr.John:The Web Weaver</b> <br> In the <b>${realm}</b> of ones and zeros, <br> Where <b>${pixels}</b> dance and ${code} unfurls, <br> There resides a silent sorcerer, <br> A <b>**${webWeaver}**</b> with dreams and <b>${pearls}</b>.`;
+let sentence2 = `<b>## I. The Blank Canvas</b> <br> <b>${name}</b> the <b>**${alchemistOfTheBrowser}**</b>, <br> Wields his <b>${keyboard}</b> like a <b>${wand}</b>, <br> He conjures <b>${pages}</b> from thin air, <br> <b>${painting}</b> landscapes with lines so fond.`;
+let sentence3 = `<b>## II. The HTML Symphony</b> <br> His <b>${fingers}</b> dance upon the <b>${keys}</b>, <br> Crafting <b>${tags}</b> like musical notes, <br> <b>${HTML}</b> whispers secrets to the breeze, <br> As <b>${name}</b> orchestrates <b>${symphonies}</b> in quotes.`;
+let sentence4 = `<b>## III. The CSS Enchanter</b> <br> Next, he dons his <b>**${CSSCloak}**</b>, <br> <b>${styling}</b> elements with flair and grace, <br> <b>${colors}</b> bloom, <b>${fonts}</b> sing, layouts <b>${evoke}</b>, <br> A <b>${visual}</b> ballet in this digital space.`;
+let sentence5 = `<b>## IV. The JavaScript Magician</b> <br> But wait, the <b>${magic}</b> deepens still, <br> <b>${name}</b> summons <b>**${JavaScriptSpells}**</b>, <br> Interactivity blooms at his <b>${will}</b>, <br> <b>${forms}</b> validate, <b>${animations} ${propel}</b>`;
+let sentence6 = `<b>## V. The Debugging Sage</b> <br> Yet, <b>${bugs}</b> creep in like shadows dark,<br> <b>${name}</b> dons his <b>**${debuggingSpectacles}**</b>, <br> Chasing <b>${glitches}</b> through the <b>${codesarc}, <br> He <b>${tames}</b> wild loops and elusive <b>${variables}</b>.`;
+let sentence7 = `<b>## VI. The Responsive Voyager</b> <br> <b>${across}</b> screens, <b>${devices}</b>, and dimensions, <br> <b>${name}</b> sails on the <b>**${responsiveSea}**</b>, <br> Media queries <b>${guide}</b> his intentions,<br> <b>${adapting}</b> layouts with <b>${fluidity}</b>.`;
+let sentence8 = `<b>## VII. The SEO Whisperer</b> <br> In the <b>${labyrinth}</b> of search engines,<br> <b>${name}</b> becomes an <b>**${SEOWhisperer}**</b>, <br> <b>${metaTags}</b>, keywords, backlink strings, <br> He crafts pathways to digital <b>${wanderers}</b>.`;
+let sentence9 = `<b>## VIII. The Git Alchemist</b> <br> Version control, a <b>${mysticalPotion}</b>, <br> <b>${name}</b> brews commits in <b>${gits}</b> cauldron, <br> Branches fork, <b>${merge}</b> conflicts in motion, <br> His <b>${repository}</b> a saga of <b>${code}</b> reborn.`;
+let sentence10 = `<b>## IX. The Deployment Dreamer</b> <br> And when the <b>${moon}</b> kisses the horizon,<br> <b>${name}</b> unfurls his creation to the <b>${world}</b>, <br> <b>${deployments}</b> like shooting stars, arisen, <br> His <b>${website}</b> sails, its sails <b>${unfurled}</b>.`;
+let sentence11 = `<b>## X. The Web Developer's Anthem</b> <br> So here's to <b>${name}</b>, the <b>**${webDeveloper}**</b>, <br> A poet of <b>${pixels}</b>, a composer of <b>${clicks}</b>, <br> In the <b>${binary}</b> symphony, he is the reveler, <br> Weaving dreams into the digital <b>${matrix}</b>.`;
+let sentence12 = `*Mr.john, may your lines be bug-free, <br> May your CSS always cascade gracefully, <br> And may your JavaScript forever flow, <br> As you navigate the web's eternal ebb and flow.*`;
+
+
+// Get the content div
+let contentDiv = document.getElementById("content");
+
+
+
+// Print the sentences to the webpage
+contentDiv.innerHTML += "<center>" + "<p>" + sentence1 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence2 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence3 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence4 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence5 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence6 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence7 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence8 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence9 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence10 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence11 + "</p>" + "</center>";
+contentDiv.innerHTML += "<center>" + "<p>" + sentence12 + "</p>" + "</center>";
+
